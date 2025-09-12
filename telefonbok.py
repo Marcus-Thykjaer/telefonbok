0a4ed1fa2fa54de9e968c7bb19ce57d26421e385
Marcus-Thykjaer <marcus@thykjaer.dk>
2025-09-12 15:10:07 +0200

    jeg laget telefonbok og la til ordbøker

---
+++ telefonbok.py
@@ -0,0 +1,15 @@
+#lager en tom telefonbok liste
+telefonbok = []
+#lager to personer og legger til dem i telefonboken
+person1 = {"navn":"sebastian","telefonnummer": "1234567"}
+person2 = {"navn":"marcus", "telefonnummer":"12356784"}
+
+
+#legger til person1 og person2 i telefonboken ved bruk av append
+telefonbok.append(person1)
+telefonbok.append(person2)
+
+print(telefonbok)
+
+
+
