8a1c946368d0ff8bb4c89957ff9958e704898735
Marcus-Thykjaer <marcus@thykjaer.dk>
2025-09-15 14:22:47 +0200

    jeg lager en funksjon som viser alle personene i listen.

--- telefonbok.py
+++ telefonbok.py
@@ -4,12 +4,17 @@ telefonbok = []
 person1 = {"navn":"sebastian","telefonnummer": "1234567"}
 person2 = {"navn":"marcus", "telefonnummer":"12356784"}
 
-
 #legger til person1 og person2 i telefonboken ved bruk av append
 telefonbok.append(person1)
 telefonbok.append(person2)
 
-print(telefonbok)
+#vi lager en funksjon til
+def vis_alle():
+    for person in telefonbok:
+        print(F"navn: {person['navn']}, telefonnummer: {person['telefonnummer']}") #f-string for å få ut navn og telefonnummer fra person
+
+#vi kaller på funksjonen
+vis_alle()
 
 
 
